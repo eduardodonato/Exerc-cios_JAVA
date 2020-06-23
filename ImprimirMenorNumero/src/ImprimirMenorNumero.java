0ad05c10ad660c6c072ce23e0a9a5804383f046a
Eduardo Almeida Donato <eduardo.donato@academico.ifpb.edu.br>
2020-06-23 15:38:58 -0300

    ImprimirMenorNumero

---
+++ ImprimirMenorNumero/src/ImprimirMenorNumero.java
@@ -0,0 +1,26 @@
+
+package exercicios;
+
+import java.util.Scanner;
+
+public class ImprimirMenorNumero {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite um numero: ");
+		int Num1 = sc.nextInt();
+		
+		System.out.println("Digite outro numero: ");
+		int Num2 = sc.nextInt();
+
+		if (Num1 < Num2) {
+			System.out.printf("O numero %d menor que o numero %d", Num1, Num2);
+		} else if (Num1 > Num2) {
+			System.out.printf("O numero %d menor que o numero %d", Num2, Num1);
+		}
+
+		sc.close();
+	}
+
+}
