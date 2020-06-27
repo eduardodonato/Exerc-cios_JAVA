e6e427f80acee8379076be1aafd053ebfbc6c6a4
Eduardo Almeida Donato <eduardo.donato@academico.ifpb.edu.br>
2020-06-27 00:07:58 -0300

    ConversorRealParaDolar

---
+++ ConversorRealParaDolar/RealParaDolar.java
@@ -0,0 +1,26 @@
+
+
+package exercicios;
+
+import java.util.Scanner;
+
+public class RealParaDolar {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe o valor da cotação atual do Dolar: ");
+		double Cotacao = sc.nextDouble();
+		
+		System.out.println("Informe qual o valor de Dolares que você possui: ");
+		double Dolar = sc.nextDouble();
+		
+		double Real = Dolar * Cotacao; 
+		
+		System.out.println("Você possui $" + Real + " Reais em dólares");
+		
+		
+		
+		sc.close();
+	}
+
+}
